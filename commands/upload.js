a2b91ec467cef3598ae40098d677ef03b13e8562
tnkrxyz <tnkrxyz@gmail.com>
2022-03-10 10:43:21 -0800

    experimenting w/ nodejs-file-downloader - cleaning up

--- commands/upload.js
+++ commands/upload.js
@@ -1,9 +1,5 @@
 const { program } = require('commander')
 const chalk = require('chalk')
-const {createWriteStream} = require('fs');
-const {pipeline} = require('stream');
-const {promisify} = require('util');
-const fetch =  require('node-fetch');
 const path = require('path');
 const Downloader = require("nodejs-file-downloader");
 
@@ -13,12 +9,12 @@ async function download({url, dir}) {
         directory: dir,
         onProgress: function (percentage, chunk, remainingSize) {
           //Gets called with each chunk.
-          console.log("% ", percentage);
+          if ( percentage % 10 == 0) {console.log(parseInt(percentage), "%");}
           //console.log("Current chunk of data: ", chunk);
           //console.log("Remaining bytes: ", remainingSize);
         },
       });
-    
+
       try {
         await downloader.download();
       } catch (error) {
@@ -26,12 +22,16 @@ async function download({url, dir}) {
       }
 }
 
+async function zim(fileName: string) {
+    console.log(`zimdumping ${fileName}... `);
+}
+}
+
 async function upload(url) {
     console.log(`Downloadinging ${url} ... `);
     await download({url: url, dir: "./"})
 
-    console.log(`zimdumping ${url}... `);
-
+    await zim(fileName);
     console.log(`Uploading ${url} to the server... `);
 }
 
