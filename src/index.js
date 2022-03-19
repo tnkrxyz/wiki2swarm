524046f9ff86deba1aee6d0f340183d888e954de
Liming Wang <lmwang@gmail.com>
2022-03-19 11:43:41 -0700

    fixed env variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const program = require('commander')
+const {program, Option} = require('commander')
 const {mirror} = require('./commands')
 
 program
@@ -13,11 +13,11 @@ program
     .option('-k, --keep-aux-files', 'keep auxillary files (jpg, media, tag, and search index)', false)
     .option('-c, --cleanup', 'Clean up downloaded and extracted files after uploading', true)
     .option('-v, --verbose', 'Print all console messages', false)
-    .option('--bee-api-url <url>', 'Bee API URL (arugment passing to swarm-cli)').env("")
-    .option('--bee-debug-api-url <url>', 'Bee Debug API URL (arugment passing to swarm-cli)').env("BEE_API_URL")
-    .option('--stamp <stamp>', 'id of stamp postage to use (arugment passing to swarm-cli)').env("BEE_DEBUG_API_URL")
-    .option('--identity <identity>', 'identity to use for "feed upload" (arugment passing to swarm-cli)').env("STAMP")
-    .option('--password <password>', 'password for using identity for "fee upload" (arugment passing to swarm-cli)').env("PASSWORD")
+    .addOption(new Option('--bee-api-url <url>', 'Bee API URL (arugment passing to swarm-cli)').env("BEE_API_URL"))
+    .addOption(new Option('--bee-debug-api-url <url>', 'Bee Debug API URL (arugment passing to swarm-cli)').env("BEE_DEBUG_API_URL"))
+    .addOption(new Option('--stamp <stamp>', 'id of stamp postage to use (arugment passing to swarm-cli)').env("STAMP"))
+    .addOption(new Option('--identity <identity>', 'identity to use for "feed upload" (arugment passing to swarm-cli)').env("IDENTITY"))
+    .addOption(new Option('--password <password>', 'password for using identity for "fee upload" (arugment passing to swarm-cli)').env("PASSWORD"))
     //.argument('[swarmOptions]', 'additional options passing to swarm-cli')
     .action(mirror);
 
